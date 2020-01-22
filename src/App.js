52fe417847b7985dc4c593267bcc6485079c79b0
Harrish Suhumar <harrish.s@hotmail.ca>
2020-01-22 15:11:16 -0500

    insert chatbot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import ChatBot from "./Components/ChatBot/ChatBot";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ChatBot />
+    </div>
+  );
 }
 
 export default App;
