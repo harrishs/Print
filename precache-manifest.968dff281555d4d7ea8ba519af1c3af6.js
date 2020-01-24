d94f1a8d19a27cc2e55bdb30bb81fed556739024
Harrish Suhumar <harrish.s@hotmail.ca>
2020-01-24 16:41:10 -0500

    Updates

---
+++ precache-manifest.968dff281555d4d7ea8ba519af1c3af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3580fdbe13ef7ee7ee9a88cd4b6ed267",
+    "url": "/Cover/index.html"
+  },
+  {
+    "revision": "6cb78ebca835c13b6812",
+    "url": "/Cover/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2e569e1a9727e9648be",
+    "url": "/Cover/static/js/2.554c830b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Cover/static/js/2.554c830b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cb78ebca835c13b6812",
+    "url": "/Cover/static/js/main.88c1b554.chunk.js"
+  },
+  {
+    "revision": "f7d47087335e5acffb8d",
+    "url": "/Cover/static/js/runtime-main.bc62ba5d.js"
+  }
+]);
